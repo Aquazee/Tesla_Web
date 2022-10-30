90fe198aeabd8a19fa3afca6802ad25eb9cc1394
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-30 11:05:56 +0530

    Added Account screen, Error Boundary

--- src/domain/ErrorBoundary/ErrorBoundary.js
+++ src/domain/ErrorBoundary/ErrorBoundary.js
@@ -3,23 +3,24 @@ import React from "react";
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: undefined };
   }
 
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { hasError: true, error: error };
   }
 
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
     console.log(error, errorInfo);
+    // this.setState({error : error })
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.error) {
       // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <h1>{(new Error(this.state.error)).message}</h1>;
     }
 
     return this.props.children; 
