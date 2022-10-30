45bccfcf0ab251102bc369172a15d2154700e01a
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-30 11:02:01 +0530

    Imported checout components

--- src/user-screens/Checkout.js
+++ src/user-screens/Checkout.js
@@ -1,6 +1,11 @@
+import { InputBox } from 'components';
+import ContactForm from '../domain/Checkout/ContactForm';
 import React, { useEffect } from 'react';
 
 import { AppFunctions } from '../utils';
+import BillingDetailsForm from 'domain/Checkout/BillingDetailsForm';
+import PaymentDetailsForm from 'domain/Checkout/PaymentDetailsForm';
+import RedeemForm from 'domain/Checkout/RedeemForm';
 
 function Checkout({ route }) {
 
@@ -34,184 +39,14 @@ function Checkout({ route }) {
             <TotalListItem />
           </ul>
 
-          <form className="">
-            <div className="input-group">
-              <input type="text" className="form-control" placeholder="Promo code" />
-              <div className="input-group-append">
-                <button type="submit" className="btn btn-secondary">Redeem</button>
-              </div>
-            </div>
-          </form>
+          <RedeemForm />
         </div>
 
         <div className="col-md-8 order-md-1">
-
-          <form className="needs-validation was-validated" noValidate="">
-            <div className='mb-1'>
-              <div className="card card-body p-1">
-                <button className="btn btn-primary bg-transparent text-dark w-100 border-0 text-left bg-white bold" type="button" data-toggle="collapse" data-target="#contactForm" aria-expanded="false" aria-controls="contactForm">
-                  Contact Details
-                </button>
-              </div>
-              <div className="collapse card card-body mb-2" id="contactForm">
-                <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="firstName">First name</label>
-                    <input type="text" className="form-control" id="firstName" placeholder="" required="" />
-                    <div className="invalid-feedback">
-                      Valid first name is required.
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="lastName">Last name</label>
-                    <input type="text" className="form-control" id="lastName" placeholder="" required="" />
-                    <div className="invalid-feedback">
-                      Valid last name is required.
-                    </div>
-                  </div>
-                </div>
-
-                <div className="mb-3">
-                  <label htmlFor="username">Username</label>
-                  <div className="input-group">
-                    <div className="input-group-prepend">
-                      <span className="input-group-text">@</span>
-                    </div>
-                    <input type="text" className="form-control" id="username" placeholder="Username" required="" />
-                    <div className="invalid-feedback" style={{ width: '100%' }}>
-                      Your username is required.
-                    </div>
-                  </div>
-                </div>
-
-                <div className="mb-3">
-                  <label htmlFor="email">Email <span className="text-muted">(Optional)</span></label>
-                  <input type="email" className="form-control" id="email" placeholder="you@example.com" />
-                  <div className="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='mb-1'>
-              <div className="card card-body p-1">
-                <button className="btn btn-primary bg-transparent text-dark w-100 border-0 text-left bg-white bold" type="button" data-toggle="collapse" data-target="#billingDetailsForm" aria-expanded="false" aria-controls="billingDetailsForm">
-                  Billing Details
-                </button>
-              </div>
-              <div className="collapse card card-body" id="billingDetailsForm">
-                <div className="mb-3">
-                  <label htmlFor="address">Address Line 1</label>
-                  <input type="text" className="form-control" id="address" placeholder="1234 Main St" required="" />
-                  <div className="invalid-feedback">
-                    Please enter your shipping address.
-                  </div>
-                </div>
-
-                <div className="mb-3">
-                  <label htmlFor="address2">Address Line 2 <span className="text-muted">(Optional)</span></label>
-                  <input type="text" className="form-control" id="address2" placeholder="Apartment or suite" />
-                </div>
-
-                <div className="row">
-                  <div className="col-md-5 mb-3">
-                    <label htmlFor="country">Country</label>
-                    <select className="custom-select d-block w-100" id="country" required="" onChange={() => console.log('selected country')}>
-                      <option value="Choose">Choose...</option>
-                      <option>United States</option>
-                    </select>
-                    <div className="invalid-feedback">
-                      Please select a valid country.
-                    </div>
-                  </div>
-                  <div className="col-md-4 mb-3">
-                    <label htmlFor="state">State</label>
-                    <select className="custom-select d-block w-100" id="state" required="" onChange={() => console.log('selected country')}>
-                      <option value="Choose">Choose...</option>
-                      <option>California</option>
-                    </select>
-                    <div className="invalid-feedback">
-                      Please provide a valid state.
-                    </div>
-                  </div>
-                  <div className="col-md-3 mb-3">
-                    <label htmlFor="zip">Zip</label>
-                    <input type="text" className="form-control" id="zip" placeholder="" required="" />
-                    <div className="invalid-feedback">
-                      Zip code required.
-                    </div>
-                  </div>
-                </div>
-                <hr className="mb-4" />
-                <div className="custom-control custom-checkbox">
-                  <input type="checkbox" className="custom-control-input" id="same-address" />
-                  <label className="custom-control-label" htmlFor="same-address">Shipping address is the same as my billing address</label>
-                </div>
-              </div>
-            </div>
-            {/* <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="save-info" />
-              <label className="custom-control-label" htmlFor="save-info">Save this information for next time</label>
-            </div> */}
-
-            <div className='mb-3'>
-              <div className="card card-body p-1 ">
-                <button className="btn btn-primary bg-transparent text-dark w-100 border-0 text-left bg-white bold" type="button" data-toggle="collapse" data-target="#paymentForm" aria-expanded="false" aria-controls="paymentForm">
-                  Payment
-                </button>
-              </div>
-              <div className="collapse card card-body" id="paymentForm">
-                <div className="d-block my-3">
-                  <div className="custom-control custom-radio">
-                    <input id="credit" name="paymentMethod" type="radio" className="custom-control-input" required="" />
-                    <label className="custom-control-label" htmlFor="credit">Credit card</label>
-                  </div>
-                  <div className="custom-control custom-radio">
-                    <input id="debit" name="paymentMethod" type="radio" className="custom-control-input" required="" />
-                    <label className="custom-control-label" htmlFor="debit">Debit card</label>
-                  </div>
-                  <div className="custom-control custom-radio">
-                    <input id="paypal" name="paymentMethod" type="radio" className="custom-control-input" required="" />
-                    <label className="custom-control-label" htmlFor="paypal">Paypal</label>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="cc-name">Name on card</label>
-                    <input type="text" className="form-control" id="cc-name" placeholder="" required="" />
-                    <small className="text-muted">Full name as displayed on card</small>
-                    <div className="invalid-feedback">
-                      Name on card is required
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="cc-number">Credit card number</label>
-                    <input type="text" className="form-control" id="cc-number" placeholder="" required="" />
-                    <div className="invalid-feedback">
-                      Credit card number is required
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-md-3 mb-3">
-                    <label htmlFor="cc-expiration">Expiration</label>
-                    <input type="text" className="form-control" id="cc-expiration" placeholder="" required="" />
-                    <div className="invalid-feedback">
-                      Expiration date required
-                    </div>
-                  </div>
-                  <div className="col-md-3 mb-3">
-                    <label htmlFor="cc-expiration">CVV</label>
-                    <input type="text" className="form-control" id="cc-cvv" placeholder="" required="" />
-                    <div className="invalid-feedback">
-                      Security code required
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
-          </form>
+          <ContactForm />
+          <BillingDetailsForm />
+          <PaymentDetailsForm />
+          <button className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
         </div>
       </div>
     </div>
