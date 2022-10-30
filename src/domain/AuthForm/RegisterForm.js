1fca50f74003e039681829077984b2586f23c608
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-10-30 11:04:21 +0530

    Structured Auth Screen to small components Added Validation

---
+++ src/domain/AuthForm/RegisterForm.js
@@ -0,0 +1,106 @@
+import { Input } from '../../components';
+import React, { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import RegisterSchema from '../../schema/register-user.schema'
+import { AppFunctions, Constants } from '../../utils'
+import SocialLoginList from './SocialLoginList';
+import UseYupValidationResolver from '../../utils/UseYupValidationResolver';
+import './style.css'
+import { useAuth } from '../../contexts';
+import Form from '../../components/Form';
+
+const RegisterForm = ({ toggleForm }) => {
+  const { signup } = useAuth()
+  const { handleSubmit, register, formState: { errors }, clearErrors } = useForm({ resolver: UseYupValidationResolver(RegisterSchema) });
+
+  const submitRegister = (data) => {
+    if (data) {
+      delete data['cpassword'];
+      const resp = signup(data);
+      toggleForm()
+    }
+  }
+
+  return (
+    <div className="form-container sign-up-container">
+      <form className='cform' name='register' onSubmit={handleSubmit((data) => submitRegister(data))}>
+        <h2>Create Account</h2>
+        <SocialLoginList />
+        <h5 style={{ color: '#cedde9' }}>OR</h5>
+        <div className='row'>
+          <div className='col-6'>
+            <Input
+              id='first_name'
+              name="first_name"
+              type="text"
+              placeholder="First Name"
+              className='cinput'
+              error={errors.first_name}
+              aria-invalid={errors.first_name ? "true" : "false"}
+              aria-required="true"
+              register={register}
+            />
+            {
+              errors.first_name && errors.last_name.type === "required" && <span className='float-left text-danger'>{errors.first_name.message}</span>
+            }
+          </div>
+          <div className='col-6'>
+            <Input
+              id='last_name'
+              name="last_name"
+              type="text"
+              placeholder="Last Name"
+              className='cinput'
+              error={errors.last_name}
+              aria-invalid={errors.last_name ? "true" : "false"}
+              aria-required="true"
+              register={register}
+            />
+            {errors.last_name && errors.last_name.type === "required" && <span className='float-left text-danger'>{errors.last_name.message}</span>}
+          </div>
+        </div>
+        <Input
+          id='email'
+          className='cinput'
+          name='email'
+          type='email'
+          placeholder="Email"
+          error={errors.email}
+          aria-invalid={errors.email ? "true" : "false"}
+          aria-required="true"
+          register={register}
+        />
+        {errors.email && errors.email.type === "required" && <span className='float-left text-danger'>{errors.email.message}</span>}
+        <Input
+          id='password'
+          className='cinput'
+          name='password'
+          type='password'
+          error={errors.password}
+          aria-invalid={errors.password ? "true" : "false"}
+          aria-required="true"
+          placeholder="Password"
+          register={register}
+        />
+        {errors.password && errors.password.type === "required" && <span className='float-left text-danger'>{errors.password.message}</span>}
+        <Input
+          id='cpassword'
+          className='cinput'
+          name='cpassword'
+          type='password'
+          error={errors.cpassword}
+          aria-invalid={errors.cpassword ? "true" : "false"}
+          aria-required="true"
+          placeholder="Confirm Password"
+          register={register}
+        />
+        {errors.cpassword && errors.cpassword.type === "required" && <span className='float-left text-danger'>{errors.cpassword.message}</span>}
+        <button className='mt-3 cbutton' id="sign-up" type='submit'>Sign Up</button>
+      </form>
+    </div>
+  )
+}
+
+export default RegisterForm;
---
+++ src/user-screens/AuthScreen.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+
+import LoginForm from '../domain/AuthForm/LoginForm'
+import RegisterForm from '../domain/AuthForm/RegisterForm'
+
+const AuthForm = () => {
+  let signUpButton, signInButton, container;
+
+  const [formErrors, setFormErrors] = useState({})
+  const removeListener = event => {
+    console.log('Removing listener');
+  };
+
+  useEffect(() => {
+    pageLoad();
+    return cleanUp();
+  })
+
+  const pageLoad = () => {
+    toggleForms();
+  }
+
+  const cleanUp = () => {
+    return [
+      signUpButton.removeEventListener('click', removeListener),
+      signInButton.removeEventListener('click', removeListener)
+    ]
+  }
+
+  const toggleForms = () => {
+    signUpButton = document.getElementById('signUp');
+    signInButton = document.getElementById('signIn');
+    container = document.getElementById('authContainer');
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+  }
+
+  const toggleForm = () => {
+    var signUpBth = document.getElementById('sign-up');
+    signUpBth.click();
+  }
+
+  return (
+    <div className="row" id="">
+      <div className="col-6 offset-3">
+        <div className="authContainer" id="authContainer">
+          <RegisterForm toggleForm={toggleForm}/>
+          <LoginForm toggleForm={toggleForm}/>
+          <OverlayContainer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AuthForm;
+
+const OverlayContainer = () => {
+  return (
+    <div className="overlay-container">
+      <div className="overlay">
+        <div className="overlay-panel overlay-left">
+          <h1>Welcome Back!</h1>
+          <p>To keep connected with us please login with your personal info</p>
+          <button className="ghost cbutton" id="signIn">Sign In</button>
+        </div>
+        <div className="overlay-panel overlay-right">
+          <h1>Hello!</h1>
+          <p>Enter your personal details and start journey with us</p>
+          <button className="ghost cbutton" id="signUp">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  )
+}
