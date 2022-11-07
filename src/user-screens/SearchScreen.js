8f07d12711d3560d8be2482533ed77efe2639023
Sudip Sharma <sudip.sharma@partner.samsung.com>
2022-11-07 10:05:19 +0530

    Setting dynamic data to Search screen

--- src/user-screens/SearchScreen.js
+++ src/user-screens/SearchScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import TabHeader from '../domain/Search/TabHeader';
 import BreadCrumb from '../domain/BreadCrumb/BreadCrumb';
 import Filters from '../domain/Filters/Filters';
 import SearchItem from '../domain/ListItem/SearchItem';
 import '../domain/Search/style.css';
+import { useProduct } from '../contexts';
 
- function SearchScreen () {
+function SearchScreen({ heading }) {
+
+  const { getProductSearchData, searchData } = useProduct();
+
   return (
     <div className="col-12">
       <div className="" style={{ padding: 10 }}>
@@ -18,7 +22,7 @@ import '../domain/Search/style.css';
             <BreadCrumb />
             <div>
               <h5 className="pddh-15 pt-2">
-                Top Budget Laptops
+                {heading}
                 <small className='pl-1'>(Showing 1 – 8 products of 8 products)</small>
               </h5>
               <div className="row">
@@ -32,7 +36,11 @@ import '../domain/Search/style.css';
                         id="one"
                         role="tabpanel"
                         aria-labelledby="one-tab">
-                        <SearchItem />
+                        {
+                          searchData && searchData.docs.map((doc, index) => {
+                            return <SearchItem key={`search_item_${index}`} {...doc} />
+                          })
+                        }
                       </div>
                     </div>
                   </div>
