34c7fafc476d893e3a70e08d7b5d76afe8d4c720
Sudip Shyamsundar  Sharma <sssharma@anadata.com>
2025-04-08 15:24:51 -0600

    Created maange address and personal information form

--- src/domain/Account/ManageAddresses.js
+++ src/domain/Account/ManageAddresses.js
@@ -1,6 +1,18 @@
+import { InputBox } from "components";
+import InputRadioGroup from "components/InputRadioGroup";
 import React from "react";
+import { useForm } from "react-hook-form";
+import Button from "components/Button";
+import LoginSchema from "schema/login-user.schema";
+import UseYupValidationResolver from "utils/UseYupValidationResolver";
 
 const ManageAddresses = () => {
+  const {
+    handleSubmit,
+    reset,
+    formState: { errors },
+    register,
+  } = useForm({ resolver: UseYupValidationResolver(LoginSchema) });
   return (
     <div
       className="tab-pane fade p-4"
@@ -9,16 +21,151 @@ const ManageAddresses = () => {
       aria-labelledby="manage_addresses-tab"
     >
       <h5 className="mb-4">Manage Addresses</h5>
-      <button type="button" id="add_address" className="btn  btn-light w-100 text-left p-3"
-        data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"
-      >+ Add A New Address</button>
+      <button
+        type="button"
+        id="add_address"
+        className="btn  btn-light w-100 text-left p-3"
+        data-toggle="collapse"
+        href="#collapseExample"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseExample"
+      >
+        + Add A New Address
+      </button>
       <div className="collapse" id="collapseExample">
         <div className="card card-body">
-          Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+          Some placeholder content for the collapse component. This panel is
+          hidden by default but revealed when the user activates the relevant
+          trigger.
         </div>
       </div>
+      <div>
+        <div className="row">
+          <InputBox
+            className="mb-3 col-4"
+            label="Name"
+            name="name"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.name && errors.name.type === "required"
+                ? errors.name.message
+                : ""
+            }
+            required
+          />
+
+          <InputBox
+            className="mb-3 col-4"
+            label="Mobile Number"
+            name="mobile"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.mobile && errors.mobile.type === "required"
+                ? errors.mobile.message
+                : ""
+            }
+            required
+          />
+        </div>
+        <div className="row">
+          <InputBox
+            className="mb-3 col-4"
+            label="Pincode"
+            name="pincode"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.pincode && errors.pincode.type === "required"
+                ? errors.pincode.message
+                : ""
+            }
+            required
+          />
+          <InputBox
+            className="mb-3 col-4"
+            label="Locality"
+            name="locality"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.locality && errors.locality.type === "required"
+                ? errors.locality.message
+                : ""
+            }
+            required
+          />
+        </div>
+        <div className="row">
+          <InputBox
+            className="mb-3 col-4"
+            label="City"
+            name="city"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.city && errors.city.type === "required"
+                ? errors.city.message
+                : ""
+            }
+            required
+          />
+          <InputBox
+            className="mb-3 col-4"
+            label="State"
+            name="state"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.state && errors.state.type === "required"
+                ? errors.state.message
+                : ""
+            }
+            required
+          />
+        </div>
+        <div className="row">
+          <InputBox
+            className="mb-3 col-4"
+            label="Landmark"
+            name="landmark"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.landmark && errors.landmark.type === "required"
+                ? errors.landmark.message
+                : ""
+            }
+          />
+          <InputBox
+            className="mb-3 col-4"
+            label="Alternate Phone"
+            name="alt_phone"
+            type="text"
+            register={register}
+            validFeedbackValue={
+              errors.alt_phone && errors.alt_phone.type === "required"
+                ? errors.alt_phone.message
+                : ""
+            }
+          />
+        </div>
+        <div className="row">
+          <InputRadioGroup
+            className="mt-3"
+            label={"Address Type"}
+            options={[
+              { label: "Home", selected: false },
+              { label: "Work", selected: false },
+            ]}
+          />
+        </div>
+        <Button className="btn-primary mt-2" value="Save"/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ManageAddresses;
--- src/domain/Account/PersonalInformation.js
+++ src/domain/Account/PersonalInformation.js
@@ -1,16 +1,106 @@
+import { InputBox } from "components";
+import Button from "components/Button";
+import InputRadioGroup from "components/InputRadioGroup";
+import constants from "../../utils/Constants";
 import React from "react";
+import { useForm } from "react-hook-form";
+import LoginSchema from "schema/login-user.schema";
+import UseYupValidationResolver from "utils/UseYupValidationResolver";
 
 const PersonalInformation = () => {
+  const {
+    handleSubmit,
+    reset,
+    formState: { errors },
+    register,
+  } = useForm({ resolver: UseYupValidationResolver(LoginSchema) });
+
+  const submitLogin = async (data) => {
+    if (data) {
+      // const resp = await login(data)
+      window.location.href = "http://localhost:3000";
+      // Notification({ notificationAlertRef, ...AppFunctions.getMessage(resp) });
+      reset();
+    }
+  };
+
+  const onEditClick = () => {};
+
   return (
     <div
-      className="tab-pane fade show active"
-      id="personal_information"
+      className="tab-pane fade show active p-4"
+      id="profile_information"
       role="tabpanel"
-      aria-labelledby="personal_information-tab"
+      aria-labelledby="profile_information-tab"
     >
-      Personal Information
+      <div className="section-header d-flex">
+        <h6 className="mb-4 mr-3 cursor-pointer">Personal Information</h6>
+        <a className="">Edit</a>
+      </div>
+      <div className="row justify-content-between">
+        <InputBox
+          className="mb-3 col-6"
+          label="First Name"
+          name="first_name"
+          type="text"
+          register={register}
+          validFeedbackValue={
+            errors.first_name && errors.first_name.type === "required"
+              ? errors.first_name.message
+              : ""
+          }
+          required
+        />
+        <InputBox
+          className="mb-3 col-6"
+          label="Last Name"
+          name="last_name"
+          type="text"
+          register={register}
+          validFeedbackValue={
+            errors.last_name && errors.last_name.type === "required"
+              ? errors.last_name.message
+              : ""
+          }
+          required
+        />
+      </div>
+      <div className="row">
+        <InputRadioGroup label={"Gender"} options={constants.GENDER} />
+      </div>
+      <div className="row">
+        <InputBox
+          className="mb-3 col-6"
+          label="Email Address"
+          name="email_address"
+          type="text"
+          register={register}
+          validFeedbackValue={
+            errors.email_address && errors.email_address.type === "required"
+              ? errors.email_address.message
+              : ""
+          }
+          required
+        />
+      </div>
+      <div className="row">
+        <InputBox
+          className="mb-3 col-6"
+          label="Mobile Number"
+          name="mobile_number"
+          type="text"
+          register={register}
+          validFeedbackValue={
+            errors.mobile_number && errors.mobile_number.type === "required"
+              ? errors.mobile_number.message
+              : ""
+          }
+          required
+        />
+      </div>
+      <Button className="btn-primary mt-2" value="Save" />
     </div>
-  )
-}
+  );
+};
 
 export default PersonalInformation;
