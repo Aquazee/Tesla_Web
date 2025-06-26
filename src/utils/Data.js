2a40f64225c71593de5cd52c53d0e601c37b82af
Sudip Shyamsundar  Sharma <sssharma@anadata.com>
2025-06-26 11:25:55 -0500

    created data.js file to get all dummy data from 1 place.

---
+++ src/utils/Data.js
@@ -0,0 +1,86 @@
+export const SPECIFICATIONS = [
+  {
+    attribute: "sales_package",
+    label: "Sales Package",
+    value:
+      "13″ MacBook Air, 35W Dual USB-C Port Power Adapter, USB-C to MagSafe 3 Cable (2 m)",
+  },
+  {
+    attribute: "model_number",
+    label: "Model Number",
+    value: "MC654HN/A",
+  },
+  {
+    attribute: "part_number",
+    label: "Part Number",
+    value: "MC654HN/A",
+  },
+  {
+    attribute: "model_name",
+    label: "Model Name",
+    value: "MacBook Air",
+  },
+  {
+    attribute: "series",
+    label: "Series",
+    value: "MacBook Air",
+  },
+  {
+    attribute: "color",
+    label: "Color",
+    value: "Silver",
+  },
+  {
+    attribute: "color",
+    label: "Type",
+    value: "Thin and Light Laptop",
+  },
+  {
+    attribute: "color",
+    label: "Suitable For",
+    value: "Processing & Multitasking",
+  },
+  {
+    attribute: "color",
+    label: "Battery Backup",
+    value: "Upto 18 Hours",
+  },
+  {
+    attribute: "color",
+    label: "Power Supply",
+    value: "35W Dual USB-C Port Power Adapter",
+  },
+  {
+    attribute: "color",
+    label: "MS Office Provided",
+    value: "No",
+  },
+];
+
+export const WARRANTY = [
+  {
+    attribute: "color",
+    label: "Warranty Summary",
+    value: "1 Year Limited Warranty",
+  },
+  {
+    attribute: "color",
+    label: "Warranty Service Type",
+    value: "Onsite",
+  },
+  {
+    attribute: "color",
+    label: "Covered in Warranty",
+    value: "Manufacturing Defects",
+  },
+  {
+    attribute: "color",
+    label: "Not Covered in Warranty",
+    value: "Physical Damage",
+  },
+  {
+    attribute: "color",
+    label: "Domestic Warranty",
+    value: "1 Year",
+  },
+];
