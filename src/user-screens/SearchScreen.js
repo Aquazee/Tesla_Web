169dd351f59a56571b2c596f3c96b538422f52cd
Sudip Shyamsundar  Sharma <sssharma@anadata.com>
2025-06-26 11:24:16 -0500

    dynamically updating search text to search page using urlparams

--- src/user-screens/SearchScreen.js
+++ src/user-screens/SearchScreen.js
@@ -1,39 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import TabHeader from '../domain/Search/TabHeader';
-import BreadCrumb from '../domain/BreadCrumb/BreadCrumb';
-import Filters from '../domain/Filters/Filters';
-import SearchItem from '../domain/ListItem/SearchItem';
-import '../domain/Search/style.css';
-import { useProduct } from '../contexts';
-import NoResultsFound from 'components/NoResultFound';
+import TabHeader from "../domain/Search/TabHeader";
+import BreadCrumb from "../domain/BreadCrumb/BreadCrumb";
+import Filters from "../domain/Filters/Filters";
+import SearchItem from "../domain/ListItem/SearchItem";
+import "../domain/Search/style.css";
+import { useProduct } from "../contexts";
+import NoResultsFound from "components/NoResultFound";
+import { getURLParams } from "utils/Helper";
 
-function SearchScreen({ heading }) {
-
+function SearchScreen({}) {
   const { getProductSearchData, searchData } = useProduct();
 
-  useEffect(() => {
+  useEffect(() => {}, []);
 
-  }, [])
-
-  console.log(searchData)
   return (
     <div className="col-12">
       <div className="" style={{ padding: 10 }}>
-        <div className="row">
-          <div className="col-3">
+        <div className="row offset-md-2">
+          <div className="col-3 col-md-2">
             <Filters />
           </div>
-          <div className="col-9 productList" style={{ padding: 0 }}>
-            <BreadCrumb />
+          <div className="col-9 col-md-8 productList" style={{ padding: 0 }}>
+            <BreadCrumb className="pl-3" />
             <div>
-              <h5 className="pddh-15 pt-2">
-                {heading}
-                <small className='pl-1'>(Showing 1 – 8 products of 8 products)</small>
+              <h5 className="pt-1 pl-3">
+                <small className="">
+                  Showing 1 – 8 products of 8 products results for "
+                  {getURLParams("search")}"
+                </small>
               </h5>
               <div className="row">
                 <div className="col-12">
-                  <div className="mt-3 ">
+                  <div className="">
                     <TabHeader />
 
                     <div className="tab-content" id="myTabContent">
@@ -41,13 +40,20 @@ function SearchScreen({ heading }) {
                         className="tab-pane fade show active p-3"
                         id="one"
                         role="tabpanel"
-                        aria-labelledby="one-tab">
-                        {
-
-                          searchData.docs.length > 0 ? searchData.docs.map((doc, index) => {
-                            return <SearchItem key={`search_item_${index}`} {...doc} />
-                          }) : <NoResultsFound />
-                        }
+                        aria-labelledby="one-tab"
+                      >
+                        {searchData.docs.length > 0 ? (
+                          searchData.docs.map((doc, index) => {
+                            return (
+                              <SearchItem
+                                key={`search_item_${index}`}
+                                {...doc}
+                              />
+                            );
+                          })
+                        ) : (
+                          <NoResultsFound />
+                        )}
                       </div>
                     </div>
                   </div>
--- src/utils/Helper.js
+++ src/utils/Helper.js
@@ -1,15 +1,20 @@
-
 export const getWindowDimensions = () => {
-  const hasWindow = typeof window !== 'undefined';
+  const hasWindow = typeof window !== "undefined";
   const width = hasWindow ? window.innerWidth : null;
   const height = hasWindow ? window.innerHeight : null;
   return {
     width,
-    height
+    height,
   };
-}
+};
 
 export const UpperCaseArray = (input) => {
-  const result = input.replace(/([A-Z]+)/g, ',$1').replace(/^,/, '');
-  return result.split(',');
-}
+  const result = input.replace(/([A-Z]+)/g, ",$1").replace(/^,/, "");
+  return result.split(",");
+};
+
+export const getURLParams = (key) => {
+  const queryString = window.location.search;
+  const params = new URLSearchParams(queryString);
+  return params.get(key);
+};
