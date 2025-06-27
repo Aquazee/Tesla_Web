f4b96a85f117589318da92df287e81d6cb984879
Sudip Sharma <sudip.test12@gmail.com>
2025-06-27 18:35:18 -0500

    added sample product and sample data for product

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -84,3 +84,171 @@ export const WARRANTY = [
     value: "1 Year",
   },
 ];
+
+export const FEATURES = [
+  {
+    name: "Processor and Memory Features",
+    features: [
+      {
+        attribute: "processor_brand",
+        label: "Processor Brand",
+        value: "Intel",
+      },
+      {
+        attribute: "processor_brand",
+        label: "Processor Name",
+        value: "Core i5",
+      },
+      {
+        attribute: "processor_brand",
+        label: "Processor Generation",
+        value: "8th Gen",
+      },
+      {
+        attribute: "processor_brand",
+        label: "SSD",
+        value: "Yes",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "SSD Capacity",
+        value: "128 GB",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "RAM",
+        value: "8 GB",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "RAM Type",
+        value: "DDR3",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "Clock Speed",
+        value: "1.6 GHz with Turbo Boost Upto 3.6 GHz",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "RAM Frequency",
+        value: "2133 MHz",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "Cache",
+        value: "4 MB",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "Graphic Processor",
+        value: "Intel Integrated UHD 617",
+      },
+      {
+        attribute: "ssd_capacity",
+        label: "Number of Cores",
+        value: "2",
+      },
+    ],
+  },
+  {
+    name: "Specifications",
+    features: SPECIFICATIONS,
+  },
+  {
+    name: "Warranty",
+    features: WARRANTY,
+  },
+];
+
+export const PRODUCT_DATA = {
+  custom_label:
+    "Apple MacBook Air Apple M4 - (24 GB/512 GB SSD/macOS Sequoia) MC654HN/A (13.6 inch, Silver, 1.24 kg)",
+  FEATURES,
+  review_and_ratings: {
+    rating: 30,
+    rated_customer_count: 20,
+    review_count: 20,
+  },
+  cost: {
+    currency: "₹",
+    mrp: 114900.0,
+    gross_amount: 52.1,
+    selling_price: 108990.0,
+    discount: 5,
+  },
+};
+
+export const PRODUCT_LIST = [
+  {
+    custom_label:
+      "Apple MacBook Pro Apple M2 Max - (32 GB/1 TB SSD/macOS Ventura) MPHG3HN/A  (14 Inch, Space Grey, 1.63 Kg)",
+    FEATURES,
+    image_link:
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/g/m/l/-original-imahayjpk2hvhayw.jpeg",
+    mobile_link: "",
+    additional_image_link: [
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/g/m/l/-original-imahayjpk2hvhayw.jpeg",
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/r/s/s/-original-imahayjpmzazpcwj.jpeg",
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/r/l/v/-original-imahayjpefquymhb.jpeg",
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/l/y/y/-original-imahayjpzcjbzgbt.jpeg",
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/0/f/t/-original-imahayjpztygbyus.jpeg",
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/t/m/8/-original-imahayjpqgzuvqgw.jpeg",
+      "https://rukminim2.flixcart.com/image/416/416/xif0q/computer/0/o/5/-original-imahayjpze9rzdtr.jpeg",
+    ],
+    review_and_ratings: {
+      rating: 4,
+      rated_customer_count: 20,
+      review_count: 0,
+    },
+    cost: {
+      currency: "₹",
+      mrp: 309900.0,
+      gross_amount: 52.1,
+      selling_price: 232999.0,
+      discount: 5,
+    },
+  },
+  {
+    custom_label:
+      "Apple MacBook AIR Apple M2 - (8 GB/256 GB SSD/Mac OS Monterey) MLY33HN/A  (13.6 Inch, Midnight, 1.24 Kg)",
+    FEATURES,
+    review_and_ratings: {
+      rating: 5,
+      rated_customer_count: 340,
+      review_count: 1167,
+    },
+    cost: {
+      currency: "₹",
+      mrp: 89900.0,
+      gross_amount: 52.1,
+      selling_price: 70990.0,
+      discount: 5,
+    },
+  },
+  {
+    custom_label:
+      "Apple MacBook Pro Intel Core i5 8th Gen - (8 GB/512 GB SSD/Mac OS Mojave) MV972HN/A  (13.3 inch, Space Grey, 1.37 kg)",
+    FEATURES,
+    image_link:
+      "https://rukminim2.flixcart.com/image/128/128/jyq5oy80/computer/t/y/2/apple-na-thin-and-light-laptop-original-imafgwev6abfznds.jpeg",
+    mobile_link: "",
+    additional_image_link: [
+      "https://rukminim2.flixcart.com/image/128/128/jyq5oy80/computer/t/y/2/apple-na-thin-and-light-laptop-original-imafgwevxrkmpqht.jpeg",
+      "https://rukminim2.flixcart.com/image/128/128/jyq5oy80/computer/t/y/2/apple-na-thin-and-light-laptop-original-imafgwev6abfznds.jpeg",
+      "https://rukminim2.flixcart.com/image/128/128/jyq5oy80/computer/t/y/2/apple-na-thin-and-light-laptop-original-imafgwevwuzkck7s.jpeg",
+    ],
+    review_and_ratings: {
+      rating: 4,
+      rated_customer_count: 19463,
+      review_count: 1167,
+    },
+    cost: {
+      currency: "₹",
+      mrp: 179900.0,
+      gross_amount: 52.1,
+      selling_price: 154999.0,
+      discount: 5,
+    },
+  },
+];
---
+++ src/utils/Urls.js
@@ -0,0 +1,5 @@
+const URLS = {
+    NO_IMAGE_FOUND: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjNwitOkVehY1hzubk6LHbM6T4JLxZ-VXYJG1ufypJiFosTCUdOTkXVpUo2wfGc2nlY3Q&usqp=CAU'
+}
+
+export default { URLS }
